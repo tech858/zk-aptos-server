1ef8cd9cdf3a5210e18f53964af187ef935471cc
tech858 <tech@laissezfairegroup.com>
2025-12-17 21:51:26 +0100

    deploy

--- src/server.js
+++ src/server.js
@@ -5,8 +5,10 @@ import cors from "cors";
 const app = express();
 import 'dotenv/config';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const appid = process.env.ZKPASS_APP_ID;
+const path = process.env.WEBHOOK;
+
 app.use(express.json());
 
 app.use(
@@ -26,41 +28,56 @@ app.get("/api/status", (req, res) => {
   });
 });
 
+
+
 // Your zkpass route
-app.get("/api/zkpass", async (req, res) => {
+app.get("/api/zkpass/:address", async (req, res) => {
+  const { address } = req.params;
+
+  if (!address) {
+    return res.status(400).json({ error: "Address is required" });
+  }
   try {
-    const appid = "397e7dfb-1e93-4a0a-a0d5-dbc2d46e5659";
+   
     const client = new zkFetchClient({ appId: appid });
     const response = await client.fetchData({
       request: {
-        url: "https://backend.zkpass.org/users/level/airdrop?address=0xecd12972e428a8256c9805b708e007882568d7d6",
+       // url: `https://backend.zkpass.org/users/level/airdrop?address=${address}`,
+       url:path+address,
         method: "GET",
-        headers: {
-          accept: "application/json, text/plain, */*",
-          "accept-encoding": "gzip, deflate, br, zstd",
-          "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-          "cache-control": "no-cache",
-          origin: "https://portal.zkpass.org",
-          pragma: "no-cache",
-          priority: "u=1, i",
-          referer: "https://portal.zkpass.org/",
-          "sec-ch-ua": '"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"',
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": '"macOS"',
-          "sec-fetch-dest": "empty",
-          "sec-fetch-mode": "cors",
-          "sec-fetch-site": "same-site",
-          "user-agent":
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
-        },
+        headers: { accept: "application/json" },
+
       },
       asserts: {
-        request: {
-          query: [
-            { key: "address", value: "0xecd12972e428a8256c9805b708e007882568d7d6" },
-          ],
-        },
-        response: [{ key: "reward", isPublic: true }],
+      
+        response: [
+            {
+            key: "dob",
+            operation: ">",
+            value: "18",
+          },
+           {
+            key:"isMale",
+            operation:"=",
+            value:"true"
+          },
+          
+           {
+            key: "verification_status",
+            operation: "=",
+            value: "valid",
+            tips:"valid KYC",
+          },
+            {
+            key: "expiry_date",
+            isPublic:true,
+          },
+          {
+            key: "country",
+            isPublic:true,
+          },
+        
+        ],
       },
     });
 
